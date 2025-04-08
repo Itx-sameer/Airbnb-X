3e9c16b7edc9d888502810a94dd4253e948d2c68
SAMEER SINGH CHAUHAN <sameersinghchauhan04@gmail.com>
2025-04-09 00:17:21 +0530

    data fillup int the footscroll

--- JS/footer.js
+++ JS/footer.js
@@ -11,8 +11,141 @@ const Data = {
         { dark: "Devonport", light: "Pet-friendly rentals", link: "#" },
         { dark: "Mallacoota", light: "Pet-friendly rentals", link: "#" },
         { dark: "ibiza", light: "Pet-friendly rentals", link: "#" }
+    ],
+
+    arts: [
+        { dark: "Phoenix", light: "Apartment rentals", link: "#" },
+        { dark: "Hot Springs", light: "Villa rentals", link: "#" },
+        { dark: "Los Angeles", light: "Rentals with pools", link: "#" },
+        { dark: "San Diego", light: "Beach apartment rentals", link: "#" },
+        { dark: "San Francisco", light: "Holiday rentals", link: "#" },
+        { dark: "Barcelona", light: "Holiday rentals", link: "#" },
+        { dark: "Prague", light: "Holiday rentals", link: "#" },
+        { dark: "Washington", light: "Flat rentals", link: "#" },
+        { dark: "Keswick", light: "Flat rentals", link: "#" },
+        { dark: "London", light: "Mansion rentals", link: "#" },
+        { dark: "Scarborough", light: "Holiday rentals", link: "#" },
+        { dark: "Sherwood Forest", light: "Cottage rentals", link: "#" },
+        { dark: "York", light: "Family-friendly rentals", link: "#" },
+        { dark: "Paris", light: "Apartment rentals", link: "#" },
+        { dark: "Rhodes", light: "Villa rentals", link: "#" },
+        { dark: "Nashville", light: "Holiday rentals", link: "#" },
+        { dark: "Dublin", light: "House rentals", link: "#" }
+    ],
+    outdoors: [
+        { dark: "Lake Martin", light: "Flat rentals", link: "#" },
+        { dark: "Banff", light: "House rentals", link: "#" },
+        { dark: "Nerja", light: "Villa rentals", link: "#" },
+        { dark: "Greer", light: "Cabin rentals", link: "#" },
+        { dark: "Lake Havasu City", light: "House rentals", link: "#" },
+        { dark: "Lake Powell", light: "Holiday rentals", link: "#" },
+        { dark: "North Rim", light: "Holiday rentals", link: "#" },
+        { dark: "Payson", light: "Holiday rentals", link: "#" },
+        { dark: "Pinetop-Lakeside", light: "Cabin rentals", link: "#" },
+        { dark: "Red Rock", light: "Cabin rentals", link: "#" },
+        { dark: "Dinner Plain", light: "Pet-friendly rentals", link: "#" },
+        { dark: "Streaky Bay", light: "Holiday rentals", link: "#" },
+        { dark: "Emerald Lake", light: "Cabin rentals", link: "#" },
+        { dark: "Vancouver Island", light: "Cottage rentals", link: "#" },
+        { dark: "Victoria", light: "Beachfront rentals", link: "#" },
+        { dark: "Idyllwild-Pine Cove", light: "House rentals", link: "#" },
+        { dark: "Mammoth Lakes", light: "Holiday rentals", link: "#" }
+    ],
+    mountains: [
+        { dark: "Mentone", light: "Cottage rentals", link: "#" },
+        { dark: "Sedona", light: "Villa rentals", link: "#" },
+        { dark: "Helen", light: "Chalet rentals", link: "#" },
+        { dark: "Pine Mountain", light: "Holiday rentals", link: "#" },
+        { dark: "Stone Mountain", light: "Cabin rentals", link: "#" },
+        { dark: "Island Park", light: "House rentals", link: "#" },
+        { dark: "Blue Mountains", light: "House rentals", link: "#" },
+        { dark: "Asheville", light: "Cottage rentals", link: "#" },
+        { dark: "Blowing Rock", light: "Apartment rentals", link: "#" },
+        { dark: "Boone", light: "Holiday rentals", link: "#" },
+        { dark: "Hochatown", light: "Holiday rentals", link: "#" },
+        { dark: "Pigeon Forge", light: "Holiday rentals", link: "#" },
+        { dark: "Townsend", light: "Holiday rentals", link: "#" },
+        { dark: "Wears Valley", light: "Holiday rentals", link: "#" },
+        { dark: "Cabins", light: "Holiday rentals", link: "#" }
+    ],
+    beach: [
+        { dark: "Dauphin Island", light: "Flat rentals", link: "#" },
+        { dark: "Fort Morgan", light: "Holiday rentals", link: "#" },
+        { dark: "Gulf Shores", light: "Holiday rentals", link: "#" },
+        { dark: "Bruny Island", light: "Holiday rentals", link: "#" },
+        { dark: "Crescent Head", light: "Beach house rentals", link: "#" },
+        { dark: "Gerringong", light: "Holiday rentals", link: "#" },
+        { dark: "Hamilton Island", light: "Flat rentals", link: "#" },
+        { dark: "Lancelin", light: "Holiday rentals", link: "#" },
+        { dark: "Melbourne Beach", light: "Beach apartment rentals", link: "#" },
+        { dark: "Moonta Bay", light: "Beachfront rentals", link: "#" },
+        { dark: "Ocean Grove", light: "Pet-friendly rentals", link: "#" },
+        { dark: "Majorca", light: "Pet-friendly rentals", link: "#" },
+        { dark: "Big Sur", light: "Cabin rentals", link: "#" },
+        { dark: "Bodega Bay", light: "Holiday rentals", link: "#" },
+        { dark: "Cambria", light: "Cabin rentals", link: "#" },
+        { dark: "Cayucos", light: "Holiday rentals", link: "#" },
+        { dark: "Huntington Beach", light: "House rentals", link: "#" }
+    ],
+    stays: [
+        { dark: "Yurt Rentals", light: "United States", link: "#" },
+        { dark: "Yurt Rentals", light: "United Kingdom", link: "#" },
+        { dark: "Castle Rentals", light: "United States", link: "#" },
+        { dark: "Houseboats", light: "United States", link: "#" },
+        { dark: "Holiday Caravans", light: "United Kingdom", link: "#" },
+        { dark: "Private Island Rentals", light: "United States", link: "#" },
+        { dark: "Farm Houses", light: "United States", link: "#" },
+        { dark: "Farm Cottages", light: "United Kingdom", link: "#" },
+        { dark: "Cabin Rentals", light: "Australia", link: "#" },
+        { dark: "Luxury Cabins", light: "United Kingdom", link: "#" },
+        { dark: "Luxury Cabins", light: "United States", link: "#" },
+        { dark: "Holiday Chalets", light: "United Kingdom", link: "#" },
+        { dark: "Cottage Rentals", light: "United States", link: "#" },
+        { dark: "Holiday Cottages", light: "United Kingdom", link: "#" },
+        { dark: "Mansion Rentals", light: "United States", link: "#" },
+        { dark: "Villa Rentals", light: "United Kingdom", link: "#" },
+        { dark: "Holiday Bungalows", light: "United Kingdom", link: "#" }
+    ],
+    categories: [
+        { dark: "Amazing pools", light: "", link: "#" },
+        { dark: "Arctic", light: "", link: "#" },
+        { dark: "Camping", light: "", link: "#" },
+        { dark: "Camper vans", light: "", link: "#" },
+        { dark: "Castles", light: "", link: "#" },
+        { dark: "Containers", light: "", link: "#" },
+        { dark: "Countryside", light: "", link: "#" },
+        { dark: "Design", light: "", link: "#" },
+        { dark: "Earth homes", light: "", link: "#" },
+        { dark: "Farms", light: "", link: "#" },
+        { dark: "National parks", light: "", link: "#" },
+        { dark: "Vineyards", light: "", link: "#" },
+        { dark: "OMG!", light: "", link: "#" },
+        { dark: "Tiny homes", light: "", link: "#" },
+        { dark: "Towers", light: "", link: "#" },
+        { dark: "Windmills", light: "", link: "#" },
+        { dark: "Luxe", light: "", link: "#" }
+    ],
+    todo: [
+        { dark: "London", light: "England", link: "#" },
+        { dark: "Paris", light: "Île-de-France", link: "#" },
+        { dark: "New York", light: "New York", link: "#" },
+        { dark: "Barcelona", light: "Catalonia", link: "#" },
+        { dark: "İstanbul", light: "İstanbul", link: "#" },
+        { dark: "Bali", light: "Indonesia", link: "#" },
+        { dark: "Amsterdam", light: "North Holland", link: "#" },
+        { dark: "Miami", light: "Florida", link: "#" },
+        { dark: "Madrid", light: "Community of Madrid", link: "#" },
+        { dark: "Los Angeles", light: "California", link: "#" },
+        { dark: "Rome", light: "Lazio", link: "#" },
+        { dark: "Lisbon", light: "Lisbon", link: "#" },
+        { dark: "Tokyo", light: "Tokyo Prefecture", link: "#" },
+        { dark: "Vienna", light: "Vienna", link: "#" },
+        { dark: "Athens", light: "Greece", link: "#" },
+        { dark: "Prague", light: "Czechia", link: "#" },
+        { dark: "Orlando", light: "Florida", link: "#" }
     ]
-}
+};
+
 function loadContent(category) {
     const grid = document.getElementById('grid');
     grid.innerHTML = "";
@@ -26,3 +159,4 @@ function loadContent(category) {
 window.onload = function () {
     loadContent('popular');
 };
+
